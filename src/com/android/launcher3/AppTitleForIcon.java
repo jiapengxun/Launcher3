242b7d4c796da1b396e8343b753286c1e0b89f18
jiapx <jiapx@jiapengxun.(none)>
2015-09-24 15:29:45 +0800

    jiapx modify customer Launcher3

---
+++ src/com/android/launcher3/AppTitleForIcon.java
@@ -0,0 +1,25 @@
+package com.android.launcher3;
+
+public class AppTitleForIcon {
+
+	public static final String NAVIGATION = "com.autonavi.xmgd.navigator";
+	public static final String TACHOGRAPH = "com.yinhang.dvr";
+	public static final String DVR_CLASSNAME = "com.yinhang.dvr.ActivityMain";
+	public static final String CAR_CLASSNAME = "com.yinhang.dvr.ActivityVideoList";
+	public static final String ECARD = "com.coagent.app";
+	public static final String MUSIC = "com.android.music";
+	public static final String VIDEO = "com.android.gallery3d";
+	public static final String FILE = "com.estrongs.android.pop";
+	public static final String MIRACAST = "com.softwinner.miracastReceiver";
+	public static final String INPUTMETHOD = "com.sohu.inputmethod.sogou";
+	
+	public static final String S_NAVIGATION = "导航";
+	public static final String S_TACHOGRAPH = "行车记录仪";
+	public static final String S_DRIVIN_VIDEO = "行车影像";
+	public static final String S_ECARD = "翼卡";
+	public static final String S_MUSIC = "音乐";
+	public static final String S_VIDEO = "视频";
+	public static final String S_FILE = "文件浏览器";
+	public static final String S_MIRACAST = "Miracast";
+	public static final String S_INPUTMETHOD = "输入法";
+}
